9fa7326f85a5302d6c55d8bd97394debb9564fc7
Adam Ladd <laddieri@gmail.com>
2018-06-26 17:28:27 -0400

    Updated button listeners

--- js/metronome.js
+++ js/metronome.js
@@ -5,45 +5,42 @@ var tempo = 120;
 //play a middle 'C' for the duration of an 8th note
 
 var colors = ["red", "orange", "blue", "green"]
+var windowCanvasRatio =1.75;
 var x=0;
-var tempoTextVar = document.getElementById("tempoText")
 var xpos=0;
 var ypos=0;
 var vdirection=-1;
 var ydirection=-1;
-var rad=40;
 var moving=0;
+var ballRatio=3;
+var rad;
 var animating=false;
 var t = 1;
 function setup() {
-  createCanvas(640, 380);
+  calculateBallSize(height);
+  createCanvas(windowWidth/windowCanvasRatio, windowHeight/windowCanvasRatio);
+  noStroke();
   ellipseMode(RADIUS);
   xpos=width/2;
   ypos=height-rad;
 }
+function windowResized() {
+  resizeCanvas(windowWidth/windowCanvasRatio, windowHeight/windowCanvasRatio);
+  calculateBallSize(height);
+  xpos = width/2;
+}
+
+function calculateBallSize (heightInput) {
+  rad = heightInput / ballRatio;
+}
 
 function draw() {
-  background(100);
+  background(255, 204, 0);
   ellipse(xpos, ypos, rad, rad);
 
-  // ypos--;
-  // ypos = ypos-(ypos*moving);
-
-  // if (animating){
-  //   if (ypos < height-(350-tempo) || ypos > height-rad){
-  //     ydirection=ydirection*-1;
-  //   }
-  //
-  //
-  //     ypos=ypos+ydirection*20;
-  //   if (ypos > height-rad){
-  //     animating=false;
-  //   }
-  //
-  // }
   if (animating){
-  updateYPos(t);
-  t++;
+    updateYPos(t);
+    t++;
   }
 }
 
@@ -62,14 +59,8 @@ function updateYPos(time){
 
 
 Tone.Transport.scheduleRepeat(function(time){
-  synth.triggerAttackRelease("C4", "8n");
+  synth.triggerAttackRelease("C4", "16n");
   Tone.Draw.schedule(function(){
-    //do drawing or DOM manipulation here
-    // document.getElementById('flashBox').style.backgroundColor = colors[x];
-    // x++;
-    // if (x==colors.length){
-    //   x=0;
-    // }
 
     animating=true;
     t=0;
@@ -86,15 +77,22 @@ function stop() {
   Tone.Transport.stop();
 }
 
-function setTempo(mtempo){
-  if(tempo>210){
-    alert("Tempo must be less than 210");
-  }
-  Tone.Transport.bpm.value = mtempo;
+function updateTempo (tempoInput) {
+  tempo = tempoInput;
+  Tone.Transport.bpm.value = tempo;
   updateTempoText(tempo);
-  document.getElementById("tempo").value = mtempo;
-  tempo=mtempo;
 }
+
+var tempoSlider = document.getElementById('tempoSlider');
+tempoSlider.addEventListener('input', function(){
+  updateTempo(this.value);
+});
+
+var tempoText = document.getElementById('tempoText');
+tempoText.addEventListener('input', function(){
+  updateTempo(this.value);
+  tempoSlider.value = this.value;
+});
 
 function updateTempoText(tempoTextInput){
   document.getElementById('tempoText').value = tempoTextInput;
