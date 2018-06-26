702b2cac5d2fc240027d7a66f9cca93e18004643
Adam Ladd <laddieri@gmail.com>
2018-06-25 20:40:33 -0400

    Bouncing ball metronome

--- js/metronome.js
+++ js/metronome.js
@@ -9,70 +9,82 @@ var x=0;
 var tempoTextVar = document.getElementById("tempoText")
 var xpos=0;
 var ypos=0;
+var vdirection=-1;
 var ydirection=-1;
 var rad=40;
 var moving=0;
+var animating=false;
+var t = 0;
 function setup() {
-  createCanvas(640, 480);
-	ellipseMode(RADIUS);
-	xpos=width/2;
-	ypos=height-rad;
+  createCanvas(640, 380);
+  ellipseMode(RADIUS);
+  xpos=width/2;
+  ypos=height-rad;
 }
 
 function draw() {
-	background(100);
+  background(100);
   ellipse(xpos, ypos, rad, rad);
 
-	// ypos--;
-	// ypos = ypos-(ypos*moving);
+  // ypos--;
+  // ypos = ypos-(ypos*moving);
 
-	updateYPos();
+  if (animating){
+    if (ypos < height-(350-tempo) || ypos > height-rad){
+      ydirection=ydirection*-1;
+    }
 
+
+      ypos=ypos+ydirection*20;
+    if (ypos > height-rad){
+      animating=false;
+    }
+
+  }
 }
 
 // function mousePressed() {
+//   animating=true;
 //
-// 	while (ypos > height/2){
-//
-// 	}
 //
 // }
 
-function updateYPos(){
-
-}
 
 Tone.Transport.scheduleRepeat(function(time){
-	synth.triggerAttackRelease("C4", "8n");
-	Tone.Draw.schedule(function(){
-	//do drawing or DOM manipulation here
-	document.getElementById('flashBox').style.backgroundColor = colors[x];
-	x++;
-	if (x==colors.length){
-		x=0;
-	}
+  synth.triggerAttackRelease("C4", "8n");
+  Tone.Draw.schedule(function(){
+    //do drawing or DOM manipulation here
+    // document.getElementById('flashBox').style.backgroundColor = colors[x];
+    // x++;
+    // if (x==colors.length){
+    //   x=0;
+    // }
 
-}, time);
+    animating=true;
+    ;
+
+  }, time);
 }, "4n");
 
 function play() {
-	Tone.Transport.stop();
-	Tone.Transport.start();
+  Tone.Transport.stop();
+  Tone.Transport.start();
 }
 
 function stop() {
-Tone.Transport.stop();
+  Tone.Transport.stop();
 }
 
-function setTempo(tempo){
-	if(tempo>210){
-		alert("Tempo must be less than 210");
-	}
-	Tone.Transport.bpm.value = tempo;
-	updateTempoText(tempo);
-	document.getElementById("tempo").value = tempo;
+function setTempo(mtempo){
+  if(tempo>210){
+    alert("Tempo must be less than 210");
+  }
+  Tone.Transport.bpm.value = mtempo;
+  updateTempoText(tempo);
+  document.getElementById("tempo").value = mtempo;
+  tempo=mtempo;
 }
 
 function updateTempoText(tempoTextInput){
-	document.getElementById('tempoText').value = tempoTextInput;
+  document.getElementById('tempoText').value = tempoTextInput;
 }
